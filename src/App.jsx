97fecfcc07c8d9b17e0acbe3bd148e0a19c5f720
webdevelopement01 <mdabusayam41@gmail.com>
2024-11-04 10:40:30 +0600

    header and display divided w

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
-  
+
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
-      
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+
+
     </>
   )
 }
